45cd0de2e286937309c8f4c820422e4c98317348
이진영 <swlsdud1005@naver.com>
2023-03-16 10:22:12 +0900

    코드 정리

--- src/context/YoutubeApiContext.jsx
+++ src/context/YoutubeApiContext.jsx
@@ -1,13 +1,11 @@
 import { createContext, useContext } from "react";
-import FakeYoutubeClient from "../api/fakeYoutubeClient.js";
 import Youtube from "../api/youtube";
 import YoutubeClient from "../api/youtubeClient";
 
 export const YoutubeApiContext = createContext();
 
-const FakeClient = new FakeYoutubeClient();
 const client = new YoutubeClient();
-const youtube = new Youtube(FakeClient);
+const youtube = new Youtube(client);
 
 export function YoutubeApiProvider({ children }) {
   return (
